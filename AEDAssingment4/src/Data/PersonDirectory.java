61ebc87755a3755cd442b061c7835c4a0b6d8595
Harshhal jaiswal <harshaljaiswal25@gmail.com>
2021-10-30 19:08:24 -0400

    assignment 4

---
+++ AEDAssingment4/src/Data/PersonDirectory.java
@@ -0,0 +1,46 @@
+package Data;
+
+import Model.Patient;
+import Model.Person;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class PersonDirectory {
+
+    private ArrayList<Person> persons;
+
+    public PersonDirectory() {
+        persons = new ArrayList<>();
+    }
+
+    public void addPerson(Person person){
+        persons.add(person);
+    }
+    
+    
+    public void removePerson(Person person){
+        persons.add(person);
+    }
+
+
+    public ArrayList<Person> getPersons() {
+        return persons;
+    }
+
+    public void setPersons(ArrayList<Person> persons) {
+        this.persons = persons;
+    }
+
+    public Person searchUser(int txtId) {
+
+        for (Person v : persons) {
+            System.out.println("person "+v.getId());
+            if (v.getId() == txtId ) {
+                System.out.println("person"+v.getFullName());
+                return v;
+            }
+        }
+        return null;
+    }
+}
---
+++ AEDAssingment4/src/Model/City.java
@@ -0,0 +1,17 @@
+package Model;
+
+public class City {
+    private String cityName;
+    
+    public City( ){
+        
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+}
